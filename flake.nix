c49a59414bdb1c451bef92112004b9719864b4c0
Exxion <exxion191@gmail.com>
2022-08-05 16:01:26 -0400

    Maybe this?

--- flake.nix
+++ flake.nix
@@ -54,15 +54,15 @@
 
           chmod +x byond
 
-          #Awful hack below to make the VS Code extension's debugger work
+          #Awful hack below to (try to) make the VS Code extension's debugger work
 
           cat $out/runbyond >> dreamseeker.exe
-          echo "exec \$WINE $out/byond/bin/dreamseeker.exe \"\$@\"" >> dreamseeker.exe
+          echo "\$WINE $out/byond/bin/dreamseeker.exe \"\$@\"" >> dreamseeker.exe
 
           chmod +x dreamseeker.exe
 
           # cat $out/runbyond >> dreammaker.exe
-          # echo "exec \$WINE $out/byond/bin/dreammaker.exe \"\$@\"" >> dreammaker.exe
+          # echo "\$WINE $out/byond/bin/dreammaker.exe \"\$@\"" >> dreammaker.exe
 
           # chmod +x dreammaker.exe
 
